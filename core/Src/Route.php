c346598632486819567afec0a262d4d2e40dba67
Тимофей Канаш <gr424_katki@lab-402-3.tomtit-tomsk.ru>
2025-05-12 11:49:52 +0700

    done to 2.6

--- core/Src/Route.php
+++ core/Src/Route.php
@@ -1,1 +1,24 @@
 <?php
+
+namespace Src;
+
+use Error;
+
+class Route{
+    private static array $routes = [];
+    private static string $prefix = '';
+
+    public static function setPrefix($value){
+        self::$prefix = $value;
+    }
+
+    public static function add(string $route, array $action): void{
+        if(!array_key_exists($route, self::$routes)){
+            self::$routes[$route] = $action;
+        }
+    }
+
+    public function start(): void{
+        $path =
+    }
+}
